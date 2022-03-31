0bd486f61f177b94ad593a6b796fcc9eedbe0ccf
john-mirage <chaosdynamix@posteo.net>
2022-03-31 19:11:36 +0200

    feat: integrate swiper for the sliders

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
 import './assets/styles/main.scss';
+import 'swiper/scss';
+import Swiper, { Navigation, Keyboard } from 'swiper';
+
+const imageSwiper = new Swiper("#slider-image", {
+    modules: [Navigation, Keyboard],
+    navigation: {
+        nextEl: '#slider-image-next',
+        prevEl: '#slider-image-prev',
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+    },
+});
+
+const textSwiper = new Swiper("#slider-text", {
+    allowTouchMove: false,
+});
+
+imageSwiper.on("slideChange", (swiper) => textSwiper.slideTo(swiper.activeIndex));
